ba4b0d086e45111d86810b2ca6d346d62faa6b0d
siguiyang <siguiyang1992@outlook.com>
2021-02-20 13:43:35 +0800

    紧急发布，通用配置servlet在gateway启动失败

--- pcloud-common/src/main/java/quick/pager/pcloud/configuration/PCloudWebMvcConfigure.java
+++ pcloud-common/src/main/java/quick/pager/pcloud/configuration/PCloudWebMvcConfigure.java
@@ -1,5 +1,6 @@
 package quick.pager.pcloud.configuration;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,6 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @author siguiyang
  */
 @Configuration
+@ConditionalOnClass(WebMvcConfigurer.class)
 public class PCloudWebMvcConfigure implements WebMvcConfigurer {
 
     @Override
