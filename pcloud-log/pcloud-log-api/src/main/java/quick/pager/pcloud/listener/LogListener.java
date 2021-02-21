467d8333ffe644637fc64136a67ce8e151e542fb
siguiyang <siguiyang1992@outlook.com>
2021-02-21 18:56:57 +0800

    打印日志

--- pcloud-log/pcloud-log-api/src/main/java/quick/pager/pcloud/listener/LogListener.java
+++ pcloud-log/pcloud-log-api/src/main/java/quick/pager/pcloud/listener/LogListener.java
@@ -2,7 +2,9 @@ package quick.pager.pcloud.listener;
 
 import com.alibaba.fastjson.JSON;
 import java.io.IOException;
+import java.time.LocalDateTime;
 import javax.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
@@ -16,6 +18,7 @@ import quick.pager.pcloud.service.LogService;
  * @author siguiyang
  */
 @Component
+@Slf4j
 public class LogListener {
 
     @Resource
@@ -23,6 +26,7 @@ public class LogListener {
 
     @StreamListener(target = LogMQ.SEND_LOG)
     public void process(@Payload String msg) throws IOException {
+        log.info("消费请求日志【{}】【{}】", LocalDateTime.now(), msg);
         LogSaveRequest logSaveReq = JSON.parseObject(msg, LogSaveRequest.class);
 
         logService.create(logSaveReq);
