f980c236589417f9a65bff574bac5e6c3327169c
siguiyang <siguiyang1992@outlook.com>
2021-04-18 15:26:13 +0800

    xxl-job定制化处理

--- pcloud-job-admin/src/main/java/com/xxl/job/admin/controller/JobLogController.java
+++ pcloud-job-admin/src/main/java/com/xxl/job/admin/controller/JobLogController.java
@@ -18,6 +18,7 @@ import com.xxl.job.core.util.DateUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -68,8 +69,8 @@ public class JobLogController {
         return new ReturnT<>(data);
     }
 
-    @RequestMapping("/getJobsByGroup")
-    public Map<String, Object> getJobsByGroup(int jobGroup) {
+    @RequestMapping("/{jobGroup}/getJobsByGroup")
+    public Map<String, Object> getJobsByGroup(@PathVariable("jobGroup") Integer jobGroup) {
         // package result
         Map<String, Object> maps = new HashMap<>();
         maps.put("code", 200);
